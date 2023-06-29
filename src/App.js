1f4643f0b314cf32ff0e2779c541c8881dbd7495
vishal desai <vishaldesai030@gmail.com>
2023-06-29 19:53:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './User.css';
 const App = () => {
   return (
     <div>
-      <h1 className='user-list'>Employee List</h1>
+      <h1 className='user-list'>Employees List</h1>
       <User />
     </div>
   );
